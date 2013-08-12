9e503929228c0b7dbe897c6140a80b9d51d950b5
David Baumgold <david@davidbaumgold.com>
2013-08-12 15:33:35 -0400

    Added a pipeline_js djangoapp
    So that we have one API endpoint that refers to all the XModule JS

---
+++ common/djangoapps/pipeline_js/templates/xmodule.js
@@ -0,0 +1,24 @@
+## This file is designed to load all the XModule Javascript files in one wad
+## using requirejs. It is passed through the Mako template system, which
+## populates the `urls` variable with a list of paths to XModule JS files.
+## These files assume that several libraries are available and bound to
+## variables in the global context, so we load those libraries with requirejs
+## and attach them to the global context manually.
+define(["jquery", "youtube", "mathjax", "codemirror", "tinymce", "jquery.tinymce", "utility"],
+       function($, YT, MathJax, CodeMirror, utility, tinymce) {
+    window.$ = $;
+    window.MathJax = MathJax;
+    window.CodeMirror = CodeMirror;
+    window.RequireJS = {
+        'requirejs': requirejs,
+        'require': require,
+        'define': define
+    };
+
+    var urls = ${urls};
+    var head = $("head");
+    $.each(urls, function(i, url) {
+        head.append($("<script/>", {src: url}));
+    });
+    return window.XModule;
+});
